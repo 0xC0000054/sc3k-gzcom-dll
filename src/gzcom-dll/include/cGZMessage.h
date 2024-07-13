91979f61941b504bc68fdc35d75e2c033b054af3
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2024-07-12 23:01:57 -0600

    Add a cGZMessage constructor overload with the data fields

--- src/gzcom-dll/include/cGZMessage.h
+++ src/gzcom-dll/include/cGZMessage.h
@@ -18,6 +18,24 @@ class cGZMessage
 			this->dwData3 = 0;
 		}
 
+		/**
+		 * A constructor that initializes a message type and sets data to zero
+		 * @param dwMessageType The type to assign to the message
+		 * @param dwData1 The data to assign to the message
+		 * @param dwData2 The data to assign to the message
+		 * @param dwData3 The data to assign to the message
+		 */
+		cGZMessage(
+			uint32_t dwMessageType,
+			uint32_t dwData1,
+			uint32_t dwData2,
+			uint32_t dwData3) {
+			this->dwMessageType = dwMessageType;
+			this->dwData1 = dwData1;
+			this->dwData2 = dwData2;
+			this->dwData3 = dwData3;
+		}
+
 		uint32_t dwMessageType;
 		uint32_t dwData1;
 		uint32_t dwData2;
