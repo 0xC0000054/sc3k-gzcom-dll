be2da7bb9a3c97e1c914c35665b3b204c9588ba2
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2025-03-22 07:15:22 -0600

    Fix the layout of cIGZApp

--- src/gzcom-dll/include/cIGZApp.h
+++ src/gzcom-dll/include/cIGZApp.h
@@ -16,25 +16,19 @@ class cIGZApp : public cIGZUnknown
 {
 	public:
 		virtual cIGZSystemService* AsIGZSystemService(void) = 0;
-		virtual char const* ModuleName(void) = 0;
-		virtual cIGZFrameWork* FrameWork(void) = 0;
-
 		virtual bool AddApplicationService(cIGZSystemService* pService) = 0;
+		virtual char const* ModuleName(void) = 0;
 
 		virtual bool PreFrameWorkInit(void) = 0;
 		virtual bool PostFrameWorkInit(void) = 0;
-		virtual bool PreFrameWorkShutdown(void) = 0;
 
 		/**
 		 * In cIGZApp, this is a no-op and always returns false.
 		 */
 		virtual bool GZRun(void) = 0;
 
-		/**
-		 * In cIGZApp, this is a no-op and always returns false.
-		 * @see cISC4App::LoadRegistry(void)
-		 */
-		virtual bool LoadRegistry(void) = 0;
+		virtual bool PreFrameWorkShutdown(void) = 0;
+		virtual cIGZFrameWork* FrameWork(void) = 0;
 
 		/**
 		 * A hardcoded point for EA/Maxis to load any dynamic libraries used
